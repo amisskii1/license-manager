74d2c78aa06a05adbaaa488f9e4b969359fa92c0
Anton Misskii <anton.misskii@gmail.com>
2024-03-30 21:15:04 +0100

    license key validation class

--- src/main/java/com/misskii/licensemanager/utils/LicenseKeyGenerator.java
+++ src/main/java/com/misskii/licensemanager/utils/LicenseKeyGenerator.java
@@ -1,67 +1,24 @@
 package com.misskii.licensemanager.utils;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
 import java.security.*;
-import java.util.Arrays;
-import java.util.Base64;
 
 @Component
-public class LicenseKeyGenerator {
-
-    public static KeyPair generateECDSAKeyPair(int keySize) throws NoSuchAlgorithmException {
-        String filename = "ecdsa_keys";
-        File keyFile = new File(filename);
-
-        if (keyFile.length() == 0) {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
-            keyPairGenerator.initialize(keySize);
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-                oos.writeObject(keyPair);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return keyPair;
-        } else {
-            // If file is not empty, load the key pair from the file
-            try {
-                return loadECDSAKeyPairFromFile(filename);
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private byte[] hashEmail(String userEmail) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        return digest.digest(userEmail.getBytes());
-    }
-
+public class LicenseKeyGenerator extends LicenseGeneral {
     private byte[] signData(byte[] data) throws Exception {
-        PrivateKey privateKey = generateECDSAKeyPair(256).getPrivate();
-        Signature signature = Signature.getInstance("SHA256withECDSA");
+        Security.addProvider(new BouncyCastleProvider());
+        PrivateKey privateKey = getECDSAKeyPair(256).getPrivate();
+        Signature signature = Signature.getInstance("SHA256withECDSA", "BC");
         signature.initSign(privateKey);
         signature.update(data);
         return signature.sign();
-    }
-
-    private String encodeBase64(byte[] data) {
-        return Base64.getEncoder().encodeToString(data);
     }
 
     public String generateLicenseKey(String userEmail) throws Exception {
         byte[] hashedEmail = hashEmail(userEmail);
         byte[] signature = signData(hashedEmail);;
         return encodeBase64(signature);
-    }
-
-    public static KeyPair loadECDSAKeyPairFromFile(String filename) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            return (KeyPair) ois.readObject();
-        }
     }
 }
--- src/main/java/com/misskii/licensemanager/utils/LicenseKeyValidator.java
+++ src/main/java/com/misskii/licensemanager/utils/LicenseKeyValidator.java
@@ -1,4 +1,20 @@
 package com.misskii.licensemanager.utils;
 
-public class LicenseKeyValidator {
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.stereotype.Component;
+
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+
+@Component
+public class LicenseKeyValidator extends LicenseGeneral{
+    public boolean verifySignature(String email, byte[] signature) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+        PublicKey publicKey = getECDSAKeyPair(256).getPublic();
+        byte[] data = hashEmail(email);
+        Signature ecdsaSignature = Signature.getInstance("SHA256withECDSA", "BC");
+        ecdsaSignature.initVerify(publicKey);
+        ecdsaSignature.update(data);
+        return ecdsaSignature.verify(signature);
+    }
 }
